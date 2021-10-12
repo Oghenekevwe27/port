99a707cb141ccfbc9a91ed56225ec6f732517709
Oghenekevwe27 <oghenekevwe27@gmail.com>
2021-10-12 03:39:47 +0100

    myfist

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Routescomp from "./Router";
+
+function App() {
+  return (
+    <div className="App">
+      <Routescomp />
+    </div>
+  );
+}
+
+export default App;
